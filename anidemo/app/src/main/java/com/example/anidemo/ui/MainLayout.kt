30f8a38cde224583c924fc66f4500e704064b297
chaosannals <chaosannals@outlook.com>
2022-09-24 16:59:02 +0800

    save

--- anidemo/app/src/main/java/com/example/anidemo/ui/MainLayout.kt
+++ anidemo/app/src/main/java/com/example/anidemo/ui/MainLayout.kt
@@ -1,11 +1,8 @@
 package com.example.anidemo.ui
 
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import android.widget.Toast
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -13,20 +10,37 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.anidemo.LocalMainScrollerPercentage
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 @ExperimentalMaterial3Api
 @Composable
 fun MainLayout() {
+    val context = LocalContext.current
     var sd by remember {
         mutableStateOf(0f)
     }
 
+    val sp by remember {
+        mutableStateOf(ScrollPercentage(0.0f){})
+    }
+
+    Observable.create {
+        sp.onChange = { f ->
+            it.onNext(f)
+        }
+        it.setCancellable {
+            Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
+        }
+    }//.subscribeOn(AndroidSchedulers.mainThread())
+        .throttleLast(400, TimeUnit.MILLISECONDS)
+        .subscribe { sd = it }
+
     CompositionLocalProvider(
-        LocalMainScrollerPercentage provides ScrollPercentage(0.0f) {
-            sd = it
-        },
+        LocalMainScrollerPercentage provides sp,
     ) {
         Scaffold(
             topBar = {},
