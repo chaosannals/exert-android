125777c9187034fd3208556ed7cf44024ac2e5cc
chaosannals <chaosannals@outlook.com>
2022-10-12 13:47:51 +0800

    save

---
+++ jcmdemo/app/src/main/java/com/example/jcmdemo/ui/page/GistItem.kt
@@ -0,0 +1,26 @@
+package com.example.jcmdemo.ui.page
+
+import com.example.jcmdemo.R
+
+
+enum class GistItem(var route: String, var icon: Int) {
+    Home("home", R.drawable.ic_home),
+    Conf("conf", R.drawable.ic_conf),
+    Camera("camera", R.drawable.ic_camera),
+    Listing("listing", R.drawable.ic_view_list),
+    Images("images", R.drawable.ic_photo_library),
+    Videos("videos", R.drawable.ic_video_library),
+    Video("video", R.drawable.ic_ondemand_video),
+    VideoRecycler("video_recycler", R.drawable.ic_featured_video),
+    SinSpray("sin_spray", R.drawable.ic_broken_image),
+    SinSpray2("sin_spray2", R.drawable.ic_broken_image),
+    ScrollDragBox("scroll_drag_box", R.drawable.ic_view_carousel),
+    ScrollCarousel("scroll_carousel", R.drawable.ic_view_carousel),
+    ScrollCarousel2("scroll_carousel2", R.drawable.ic_view_carousel),
+    PathDataParser("path_data_parser", R.drawable.ic_image),
+    WebViewBox("web_view_box", R.drawable.ic_web),
+    WebViewX5Box("web_view_x5_box", R.drawable.ic_web),
+//    Conf1("conf", R.drawable.ic_conf),
+//    Conf2("conf", R.drawable.ic_conf),
+//    Conf3("conf", R.drawable.ic_conf),
+}
--- jcmdemo/app/src/main/java/com/example/jcmdemo/ui/page/GistPage.kt
+++ jcmdemo/app/src/main/java/com/example/jcmdemo/ui/page/GistPage.kt
@@ -19,35 +19,12 @@ import com.example.jcmdemo.LocalNavController
 import com.example.jcmdemo.R
 import com.example.jcmdemo.ui.routeTo
 
-enum class GistItem(var route: String, var icon: Int) {
-    Home("home", R.drawable.ic_home),
-    Conf("conf", R.drawable.ic_conf),
-    Camera("camera", R.drawable.ic_camera),
-    Listing("listing", R.drawable.ic_view_list),
-    Images("images", R.drawable.ic_photo_library),
-    Videos("videos", R.drawable.ic_video_library),
-    Video("video", R.drawable.ic_ondemand_video),
-    VideoRecycler("video_recycler", R.drawable.ic_featured_video),
-    SinSpray("sin_spray", R.drawable.ic_gist),
-    SinSpray2("sin_spray2", R.drawable.ic_gist),
-    ScrollDragBox("scroll_drag_box", R.drawable.ic_gist),
-    ScrollCarousel("scroll_carousel", R.drawable.ic_gist),
-    ScrollCarousel2("scroll_carousel2", R.drawable.ic_gist),
-    PathDataParser("path_data_parser", R.drawable.ic_gist),
-    WebViewBox("web_view_box", R.drawable.ic_gist),
-    WebViewX5Box("web_view_x5_box", R.drawable.ic_gist),
-//    Conf1("conf", R.drawable.ic_conf),
-//    Conf2("conf", R.drawable.ic_conf),
-//    Conf3("conf", R.drawable.ic_conf),
-}
-
 @Composable
 fun GistPage () {
     val items = GistItem.values()
     val columnCount = 3
 
     val navController = LocalNavController.current
-
 
     Layout(
         // 修改器会影响到子空间的测量，使得子修改器失效
