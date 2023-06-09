579778c250c7424d07dc4e38a89a7a3e873c63c0
chaosannals <chaosannals@outlook.com>
2023-06-09 11:40:02 +0800

    save

--- AppShell/app24/src/main/java/com/example/app24/ui/MainView.kt
+++ AppShell/app24/src/main/java/com/example/app24/ui/MainView.kt
@@ -1,6 +1,7 @@
 package com.example.app24.ui
 
 import android.util.Log
+import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
@@ -19,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.zIndex
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -26,6 +28,7 @@ import com.example.app24.ScaffoldKit
 import com.example.app24.ui.widget.BottomBar
 import com.example.app24.ui.widget.FloatBall
 import com.example.app24.ui.widget.X5WebView
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
 @Composable
@@ -40,12 +43,39 @@ fun MainView() {
         }
     }
 
-    var floatX by remember {
-        mutableStateOf(Design.displayWidth - 40f.sf)
+    // 无法作为 key 的 问题，导致此处只是个保留对比值
+    var floatSize by remember {
+        Log.d("FloatBall", "floatSize init")
+        mutableStateOf(IntSize.Zero)
     }
-    var floatY by remember {
-        mutableStateOf(Design.displayHeight - 140f.sf)
+    var floatRight by remember {
+        mutableStateOf(0f)
     }
+    var floatBottom by remember {
+        mutableStateOf(0f)
+    }
+
+    // 日志上看，数值有变动，但是加入 floatSize key 后 floatX 数值改变了不起效 offset
+    //var floatX by remember(floatSize) {
+//    var floatX by remember {
+//        Log.d("FloatBall", "floatX init")
+////        mutableStateOf((Design.displayWidth - floatSize.width).toFloat())
+//        mutableStateOf(0f)
+//    }
+    val floatX = remember {
+        Animatable(Design.displayWidth.toFloat())
+    }
+//    var floatY by remember {
+//        Log.d("FloatBall", "floatY init")
+//        //mutableStateOf(Design.displayHeight - floatSize.height - 40f.sf)
+//        mutableStateOf(0f)
+//    }
+    val floatY = remember {
+        Animatable(Design.displayHeight.toFloat())
+    }
+
+    // TODO 动画用到，找个好看的写法
+    val scope = rememberCoroutineScope()
 
     CompositionLocalProvider(
         LocalNavController provides navController
@@ -101,15 +131,41 @@ fun MainView() {
                     .align(Alignment.TopStart)
                     .offset {
                         IntOffset(
-                            floatX.roundToInt(),
-                            floatY.roundToInt(),
+                            floatX.value.roundToInt(),
+                            floatY.value.roundToInt(),
                         )
-                    }.pointerInput(Unit) {
-                        detectDragGestures { change, dragAmount ->
-                            change.consume()
-                            floatX = (floatX + dragAmount.x).coerceIn(0f, Design.displayWidth - 40f.sf)
-                            floatY = (floatY + dragAmount.y).coerceIn(0f, Design.displayHeight - 40f.sf)
+                    }
+                    .pointerInput(Unit) {
+                        if (floatSize != size) {
+                            Log.d("FloatBall", "$floatSize $size")
+                            floatSize = size
+                            floatX.snapTo((Design.displayWidth - floatSize.width).toFloat())
+                            floatY.snapTo(Design.displayHeight - floatSize.height - 40f.sf)
+                            floatRight = (Design.displayWidth - size.width).toFloat()
+                            floatBottom = Design.displayHeight - size.height - 40f.sf
                         }
+
+                        detectDragGestures(
+                            onDragEnd =
+                            {
+                                scope.launch {
+                                    Log.d("FloatBall", "FloatBall End ${floatX.value} $floatRight $size")
+                                    if (floatX.value > (floatRight / 2)) {
+                                        floatX.animateTo(floatRight)
+                                    } else {
+                                        floatX.animateTo(0f)
+                                    }
+                                }
+                            },
+                            onDrag = { change, dragAmount ->
+                                Log.d("FloatBall", "$floatX $floatY")
+                                change.consume()
+                                scope.launch { // TODO 找一个好看的写法
+                                    floatX.snapTo((floatX.value + dragAmount.x).coerceIn(0f, floatRight))
+                                    floatY.snapTo((floatY.value + dragAmount.y).coerceIn(0f, floatBottom))
+                                }
+                            },
+                        )
                     },
             )
         }
