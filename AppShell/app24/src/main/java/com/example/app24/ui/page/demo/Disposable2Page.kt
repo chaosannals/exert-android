c3f197df2ef76e0351ee95f1810e3aacc8084eae
chaosannals <chaosannals@outlook.com>
2023-07-17 11:49:47 +0800

    save

--- AppShell/app24/src/main/java/com/example/app24/ui/page/demo/Disposable2Page.kt
+++ AppShell/app24/src/main/java/com/example/app24/ui/page/demo/Disposable2Page.kt
@@ -1,10 +1,14 @@
 package com.example.app24.ui.page.demo
 
 import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.app24.ui.DesignPreview
 import com.example.app24.ui.LocalNavController
@@ -20,9 +24,17 @@ fun Disposable2Page() {
             Log.d("app24", "Disposable2Page onDispose")
         }
     }
-
-    Button(onClick = { navController.navigate("disposable-page") }) {
-        Text("To 1")
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+    ) {
+        Button(onClick = { navController.navigate("disposable-2-page") }) {
+            Text("To Self")
+        }
+        Button(onClick = { navController.navigate("disposable-page") }) {
+            Text("To 1")
+        }
     }
 }
 
--- AppShell/app24/src/main/java/com/example/app24/ui/page/demo/DisposablePage.kt
+++ AppShell/app24/src/main/java/com/example/app24/ui/page/demo/DisposablePage.kt
@@ -1,10 +1,14 @@
 package com.example.app24.ui.page.demo
 
 import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.app24.ui.DesignPreview
 import com.example.app24.ui.LocalNavController
@@ -13,6 +17,10 @@ import com.example.app24.ui.LocalNavController
 /// 进入 DisposablePage 再切换 Disposable2Page 的顺序是即：
 /// 1. DisposablePage Compose
 /// 2. Disposable2Page Compose
+/// 3. DisposablePage onDispose
+/// 路由切换自身也是如此，即：
+/// 1. DisposablePage Compose
+/// 2. DisposablePage Compose
 /// 3. DisposablePage onDispose
 
 @Composable
@@ -27,8 +35,17 @@ fun DisposablePage() {
         }
     }
 
-    Button(onClick = { navController.navigate("disposable-2-page") }) {
-        Text("To 2")
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+    ) {
+        Button(onClick = { navController.navigate("disposable-page") }) {
+            Text("To Self")
+        }
+        Button(onClick = { navController.navigate("disposable-2-page") }) {
+            Text("To 2")
+        }
     }
 }
 
