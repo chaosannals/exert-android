fae1aca4256056513b0769c7177d07e132433f0d
chaosannals <chaosannals@outlook.com>
2023-07-17 11:15:57 +0800

    save

--- AppShell/app24/src/main/java/com/example/app24/js/JsSdk.kt
+++ AppShell/app24/src/main/java/com/example/app24/js/JsSdk.kt
@@ -1,8 +1,21 @@
 package com.example.app24.js
 
+import android.os.Parcelable
+import android.util.Log
 import android.webkit.JavascriptInterface
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
+
+@Parcelize
+@Serializable
+data class JsSdkObject(
+    val keyA: String,
+    val keyB: Boolean,
+) : Parcelable
+
+/// @JavascriptInterface 的返回值只能是基础类型 int String
+///
 class JsSdk (
     val onSyncCall: (String, String) -> Unit,
     val onSyncFail: (String, String) -> Unit,
@@ -11,6 +24,36 @@ class JsSdk (
     val onResultCall: (String, String) -> Unit,
     val onResultFail: (String, String) -> Unit,
 ) {
+    // 不可返回 Map,前端拿到是空对象 {}
+    @JavascriptInterface
+    fun getMap() : Map<String, Boolean> {
+        val result = mutableMapOf<String, Boolean>()
+        for (c in 'a'..'z') {
+            result.put("$c key", true)
+        }
+        Log.d("appsbf", "jssdk getMap $result")
+        return result
+    }
+
+    // 不可返回自定义,前端拿到是空对象 {}
+    @JavascriptInterface
+    fun getObject(): JsSdkObject {
+        return JsSdkObject(
+            keyA = "12345",
+            keyB = true,
+        )
+    }
+
+    @JavascriptInterface
+    fun getString(): String {
+        return "string result"
+    }
+
+    @JavascriptInterface
+    fun getInt(): Int {
+        return 123456
+    }
+
     @JavascriptInterface
     fun launchDispatch(uuid: String, name: String, param: String) {
         when(name) {
