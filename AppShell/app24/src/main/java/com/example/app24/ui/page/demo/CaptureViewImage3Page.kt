7e17462176d5cd232eeb35ab50bb1f1d8d949e73
chaosannals <chaosannals@outlook.com>
2023-07-21 18:00:55 +0800

    save

---
+++ AppShell/app24/src/main/java/com/example/app24/ui/page/demo/CaptureViewImage3Page.kt
@@ -0,0 +1,9 @@
+package com.example.app24.ui.page.demo
+
+import androidx.compose.runtime.Composable
+
+// TODO xblur-compose PixelCopy 实现截图
+// 采用此方式 替换 onDraw
+@Composable
+fun CaptureViewImage3Page() {
+}
